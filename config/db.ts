58791cd183290c00ee17b43f15d10a976fce6965
Gareleon <dragan.ignjatovic.student@gmail.com>
2025-02-16 19:19:23 +0100

    feat: configured MongoDB connection

---
+++ config/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    //Remove the error for strict query
+    mongoose.set("strictQuery", true);
+    const DB_URL = `${process.env.DB_URL_BASE}:${process.env.SECRET_KEY}${process.env.DB_HOST}`;
+    await mongoose.connect(DB_URL);
+    console.log("✅ MongoDB connected successfully!");
+  } catch (err) {
+    console.error("❌ Error connecting to MongoDB:", err);
+    process.exit(1);
+  }
+};
