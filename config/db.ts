3e43d5cbfbddc1fe9f6e4b3a5804efa5ab5a60e0
Gareleon <dragan.ignjatovic.student@gmail.com>
2025-02-16 19:47:58 +0100

    changed message for outcome of connection with DB

--- config/db.ts
+++ config/db.ts
@@ -6,9 +6,9 @@ export const connectDB = async () => {
     mongoose.set("strictQuery", true);
     const DB_URL = `${process.env.DB_URL_BASE}:${process.env.SECRET_KEY}${process.env.DB_HOST}`;
     await mongoose.connect(DB_URL);
-    console.log("✅ MongoDB connected successfully!");
+    console.log("MongoDB connected successfully!");
   } catch (err) {
-    console.error("❌ Error connecting to MongoDB:", err);
+    console.error("Error connecting to MongoDB:", err);
     process.exit(1);
   }
 };
